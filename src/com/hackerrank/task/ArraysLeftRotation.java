3bba749808f912073b60c474106b50167bb4babb
rudip <rudi.purnomo@quadras.co.id>
2022-06-15 17:49:17 +0700

    delete

--- src/com/hackerrank/task/ArraysLeftRotation.java
+++
@@ -1,9 +0,0 @@
-package com.hackerrank.task;
-
-public class ArraysLeftRotation {
-	
-//	public void main(String[] args) {
-//		
-//	}
-
-}
--- src/com/hackerrank/task/CountGwith2Dimension.java
+++
@@ -1,7 +0,0 @@
-package com.hackerrank.task;
-
-public class CountGwith2Dimension {
-	
-	
-
-}
